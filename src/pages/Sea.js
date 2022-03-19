7ea385676c95594f723da1c87683c4a04668292d
karimfawaz <38626543+karimfawaz@users.noreply.github.com>
2022-03-19 01:26:37 +0000

    Pretty Push

--- src/pages/Sea.js
+++ src/pages/Sea.js
@@ -1,14 +1,12 @@
 import { useWeather } from "../hooks/useWeather";
 import { LoadingSpinner } from "../components/LoadingSpinner";
 import "./Sea.scss";
-import uvi from "../assets/uvi.png"
-import dew from "../assets/dew.png"
+import uvi from "../assets/uvi.png";
+import dew from "../assets/dew.png";
 import humidity from "../assets/humidity.png";
-import atmpressure from "../assets/atmpressure.png"
-import clouds from "../assets/clouds.png"
-import visibility from "../assets/visibility.png"
-
-
+import atmpressure from "../assets/atmpressure.png";
+import clouds from "../assets/clouds.png";
+import visibility from "../assets/visibility.png";
 
 export const Sea = () => {
   const data = useWeather();
@@ -51,8 +49,7 @@ export const Sea = () => {
         <h3 className="card-title">Clouds</h3>
         <h5 className="card-value">{data.current.clouds} </h5>
       </div>
-     
-     
+
       {/* {data.current.pressure}
       {data.current.humidity}
       {data.current.dew_point}
