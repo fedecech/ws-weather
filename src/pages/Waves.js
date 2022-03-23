9225bb28e85e617c6a55d46b8e4d143ad9f38ca9
Federico Cecchinato <federicocech@gmail.com>
2022-03-23 12:44:55 +0000

    wave page

--- src/pages/Waves.js
+++ src/pages/Waves.js
@@ -1,10 +1,52 @@
-import { useStormGlass } from '../hooks/useStormGlass';
+import { mockData, useStormGlass } from '../hooks/useStormGlass';
 import { useCurrentWeather } from '../hooks/useCurrentWeather';
+import { ArrowDownIcon } from "@heroicons/react/outline"
+import "./Waves.scss"
+const data = mockData;
+const days = [
+  "Su",
+  "Mo",
+  "Tu",
+  "We",
+  "Th",
+  "Fr",
+  "Sa"
+]
 
 export const Waves = () => {
-  const w = useCurrentWeather();
+  // const w = useCurrentWeather();
 
-  const s = useStormGlass(w?.coords?.lat, w?.coords?.lon);
-  console.log(s);
-  return <div>Waves</div>;
+
+  return (
+    <div style={{ width: '90%', height: '100%', alignSelf: 'center', overflow: 'auto' }}>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Swell Height</th>
+            <th>Swell Period</th>
+            <th>Water Temperature</th>
+            <th>Wave Direction</th>
+            <th>Wave Height</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.hours.map(d => {
+            const date = new Date(d.time)
+            const day = date.getDay()
+            return (
+              <tr>
+                <td>{days[day]} {day} {date.getHours()}h</td>
+                <td>{parseFloat(d.swellHeight.sg).toFixed(1)}</td>
+                <td>{parseFloat(d.swellPeriod.sg).toFixed(1)}</td>
+                <td>{parseFloat(d.waterTemperature.sg).toFixed(1)}</td>
+                <td><ArrowDownIcon style={{ width: '15px', height: '15px', transform: `rotate(${d.waveDirection.sg}deg)` }} /></td>
+                <td>{parseFloat(d.waveHeight.sg).toFixed(1)}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
